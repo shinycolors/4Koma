922968c4f31200811a074d0512e8d1be18c956d8
MPThLee <mpthlee@gmail.com>
2018-05-09 13:05:40 +0900

    Improve lints.

--- modules/typescript.js
+++ modules/typescript.js
@@ -4,26 +4,34 @@ module.exports = function(options) {
 
   // Extend build
   this.extendBuild(config => {
-    const tsLoader = {
+    config.module.rules.push({
+      enforce: "pre",
+      test: /\.tsx?$/,
+      loader: "tslint-loader",
+      exclude: /(node_modules)/,
+      options: {
+        tsConfigFile: "tsconfig.json",
+        configFile: "tslint.json"
+      }
+    });
+    // Add TypeScript loader
+    config.module.rules.push({
+      test: /((client|server)\.js)|(\.tsx?)$/,
       loader: "ts-loader",
       options: {
+        configFile: "tsconfig.json",
         appendTsSuffixTo: [/\.vue$/]
       }
-    };
-    // Add TypeScript loader
-    config.module.rules.push(
-      Object.assign(
-        {
-          test: /((client|server)\.js)|(\.tsx?)$/
-        },
-        tsLoader
-      )
-    );
+    });
     // Add TypeScript loader for vue files
     for (let rule of config.module.rules) {
       if (rule.loader === "vue-loader") {
-        rule.options.loaders.ts = tsLoader;
+        rule.options.loaders.ts = "ts-loader!tslint-loader";
       }
+    }
+    // Add .ts extension in webpack resolve
+    if (config.resolve.extensions.indexOf(".ts") === -1) {
+      config.resolve.extensions.push('.ts')
     }
   });
 };
